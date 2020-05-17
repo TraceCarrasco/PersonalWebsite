eda7e1eda32d91357543210ea875207154730166
TraceCarrasco <messagetrace@gmail.com>
2020-05-17 18:03:21 -0500

    Initial commit

---
+++ src/Home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+class Home extends React.Component {
+  render() {
+    return <h1> Hello from Home!</h1>
+  }
+}
